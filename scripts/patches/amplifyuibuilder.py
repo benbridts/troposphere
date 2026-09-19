d27440b78708f8b8aee5fb7208bcde5f41b4e229
Mark Peek <mark@peek.org>
2026-09-19 09:11:08 -0700

    Add missing services

--- scripts/patches/amplifyuibuilder.py
+++ scripts/patches/amplifyuibuilder.py
@@ -9,4 +9,34 @@ patches = [
         "path": "/PropertyTypes/AWS::AmplifyUIBuilder::Component.ComponentConditionProperty/Properties/Then/Type",
         "value": "object",
     },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::AmplifyUIBuilder::Component.ComponentProperty/Properties/Concat/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::AmplifyUIBuilder::Component.Predicate/Properties/And/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::AmplifyUIBuilder::Component.Predicate/Properties/Or/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::AmplifyUIBuilder::Component.ComponentChild/Properties/Children/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::AmplifyUIBuilder::Form.FormInputValueProperty/Properties/Concat/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::AmplifyUIBuilder::Theme.ThemeValue/Properties/Children/ItemType",
+        "value": "object",
+    },
 ]
---
+++ scripts/patches/bcm.py
@@ -0,0 +1,27 @@
+patches = [
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::BCM::Dashboard.CostAndUsageExpression/Properties/And/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::BCM::Dashboard.CostAndUsageExpression/Properties/Not/Type",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::BCM::Dashboard.CostAndUsageExpression/Properties/Or/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::BCM::Dashboard.Expression/Properties/And/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::BCM::Dashboard.Expression/Properties/Not/Type",
+        "value": "object",
+    },
+]
