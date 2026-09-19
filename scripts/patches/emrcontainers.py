1b7d82b284e2d48ce6c9bcc21e595c1dd7e172e4
Mark Peek <mark@peek.org>
2026-09-19 07:44:31 -0700

    Implement patches for 262.0.0
    Fixups applied for:
    - AWS::EMRContainers::JobRun
    - AWS::EMRServerless::JobRun

--- scripts/patches/emrcontainers.py
+++ scripts/patches/emrcontainers.py
@@ -4,4 +4,24 @@ patches = [
         "path": "/PropertyTypes/AWS::EMRContainers::Endpoint.EMREKSConfiguration/Properties/Configurations/ItemType",
         "value": "object",
     },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::EMRContainers::JobRun.Configuration/Properties/Configurations/ItemType",
+        "value": "object",
+    },
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::EMRContainers::JobRun.ConfigurationOverrides",
+        "path": "/PropertyTypes/AWS::EMRContainers::JobRun.JobRunConfigurationOverrides",
+    },
+    {
+        "op": "move",
+        "from": "/ResourceTypes/AWS::EMRContainers::JobRun/Properties/ConfigurationOverrides",
+        "path": "/ResourceTypes/AWS::EMRContainers::JobRun/Properties/JobRunConfigurationOverrides",
+    },
+    {
+        "op": "replace",
+        "path": "/ResourceTypes/AWS::EMRContainers::JobRun/Properties/JobRunConfigurationOverrides/Type",
+        "value": "JobRunConfigurationOverrides",
+    },
 ]
--- scripts/patches/emrserverless.py
+++ scripts/patches/emrserverless.py
@@ -3,4 +3,9 @@ patches = [
         "op": "remove",
         "path": "/PropertyTypes/AWS::EMRServerless::Application.ConfigurationObject/Properties/Configurations",
     },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::EMRServerless::JobRun.Configuration/Properties/Configurations/ItemType",
+        "value": "object",
+    },
 ]
